82351157db99b0cac657e978726d845540ae5358
Ananya Nair <an.nair1702@gmail.com>
2022-10-23 21:01:09 +0530

    Created board framework and 'Add Task' feature

---
+++ script.js
@@ -0,0 +1,11 @@
+function Show(){
+    var x ;
+    
+    x=document.getElementById("create-new-task-block").style.display="flex";
+}
+
+function Hide(){
+    var y ;
+    
+    y=document.getElementById("create-new-task-block").style.display="none";
+}
