9df1121dd215270e776cfedd68ec2980bfd18404
Ananya Nair <an.nair1702@gmail.com>
2022-10-24 15:06:53 +0530

    Created menu popup and added overlay for task popup

--- script.js
+++ script.js
@@ -2,10 +2,25 @@ function Show(){
     var x ;
     
     x=document.getElementById("create-new-task-block").style.display="flex";
+    x=document.getElementById("overlay").style.display="block";
 }
 
 function Hide(){
     var y ;
     
     y=document.getElementById("create-new-task-block").style.display="none";
+    y=document.getElementById("overlay").style.display="none";
+}
+
+function openmenu(){
+    var z;
+
+    z=document.getElementById("myDropdown").style.display="flex";
+    
+}
+
+function closemenu(){
+    var m;
+
+    m=document.getElementById("myDropdown").style.display="none";
 }
